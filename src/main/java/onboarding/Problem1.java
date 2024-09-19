827f50f32ed138714965aca33d4598fb873b72ed
ehgur <ehgur062300@gmail.com>
2024-09-19 14:35:28 +0900

    feat: PROBLEM1 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,46 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    private static boolean isException(List<Integer> pages){
+        if(pages.get(0) < 1 || pages.get(1) > 400 || pages.get(0) > pages.get(1))
+            return true;
+        return Math.abs(pages.get(1) - pages.get(0)) > 1;
+    }
+
+    private static int findMaxValueByPages(List<Integer> pages){
+        int left = findMaxValueByPage(pages.get(0));
+        int right = findMaxValueByPage(pages.get(1));
+
+        return Math.max(left,right);
+    }
+
+    private static int findMaxValueByPage(int page){
+        int sum = 0, mul = 1;
+
+        while(page > 0){
+            int cur = page%10;
+            sum += cur;
+            mul *= cur;
+            page /= 10;
+        }
+
+        return Math.max(sum,mul);
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        int answer;
+
+        if(isException(pobi) || isException(crong))
+            return -1;
+
+        if(findMaxValueByPages(pobi) > findMaxValueByPages(crong))
+            answer = 1;
+        else if(findMaxValueByPages(pobi) < findMaxValueByPages(crong))
+            answer = 2;
+        else
+            answer = 0;
+
         return answer;
     }
 }
