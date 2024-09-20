f15a5fc5e12174d208f9929ef6f7084e2c35a0ab
ehgur <ehgur062300@gmail.com>
2024-09-20 14:01:39 +0900

    feat: Problem4 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,31 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        Problem4 T = new Problem4();
+        return T.convertWord(word);
     }
+
+    private String convertWord(String word){
+        StringBuilder sb = new StringBuilder();
+        for (Character ch : word.toCharArray()) {
+            if(ch == ' ')
+                sb.append(' ');
+            else if(ch > 90)
+                sb.append(lowerConvert(ch));
+            else
+                sb.append(upperConvert(ch));
+        }
+        return sb.toString();
+    }
+
+    private char upperConvert(char c){
+        int num = 155-(int)c;
+        return (char)num;
+    }
+
+    private char lowerConvert(char c){
+        return (char) (219-(int)c);
+    }
+
+
 }
