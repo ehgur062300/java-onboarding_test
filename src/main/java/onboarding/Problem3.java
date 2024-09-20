d2c4a9a9939b24032dec044826a630a3b703b56d
ehgur <ehgur062300@gmail.com>
2024-09-20 13:23:44 +0900

    feat: Problem3 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,25 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        return gameResult(number);
+    }
+
+    private static int gameResult(int number){
+        int clap = 0;
+        for(int i=1; i<=number; i++){
+            int cnt = 0;
+            clap += clapCount(i,cnt);
+        }
+        return clap;
+    }
+
+    private static int clapCount(int number, int cnt){
+        while(number > 0){
+            int n = number % 10;
+            if(n != 0 && n%3 == 0)
+                cnt++;
+            number /= 10;
+        }
+        return cnt;
     }
 }
