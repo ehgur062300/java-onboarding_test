6605b19f748589da743e8945a4db4bf54513e827
ehgur <ehgur062300@gmail.com>
2024-09-21 22:00:51 +0900

    feat: Problem5 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,23 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
+    private static final int[] moneys = {50000,10000,5000,1000,500,100,50,10,1};
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        Problem5 T = new Problem5();
+        return T.wallet(money);
+    }
+
+    private List<Integer> wallet(int money){
+        List<Integer> divisionMoney = new ArrayList<>();
+
+        for(int m : moneys){
+            divisionMoney.add(money/m);
+            money %= m;
+        }
+        return divisionMoney;
     }
 }
